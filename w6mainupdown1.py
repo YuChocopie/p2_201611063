5b2f48515e1812d89bfd0c4924974516fbdc1d56
dbwjd <dbwjd@users.noreply.github.com>
2016-04-07 19:36:33 +0900

    up and down

---
+++ w6mainupdown1.py
@@ -0,0 +1,19 @@
+﻿def UpDown():
+    man=input("please write number : ")
+    chance=input("please write chance : ")
+    for i in range(chance):
+        woman=input("please write number : ")
+        if man>woman :
+            print "Up"
+        elif man<woman:
+            print "Down"
+        elif man==woman:
+            print "Ding~ Dong~ Dang~"
+            break
+    print "Game over"     
+def lab6():
+    UpDown()
+def main():
+    lab6()
+if __name__=="__main__":
+    main()
