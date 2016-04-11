9346bc7e9d3f4dca6ae011ee1d86ba04ca93d43f
dbwjd <dbwjd@users.noreply.github.com>
2016-04-11 18:01:21 +0900

    4.11 3의배수 5의배수제외

---
+++ w6mainupdown12.py
@@ -0,0 +1,21 @@
+﻿def sumList(aList):
+    x=list()
+    mysum=0
+    for i in range(0,aList):
+        if i%4==0 and not i%5==0:
+            mysum+=i
+    return mysum
+
+
+def lab6():
+    """my homework!!!!"""
+    aList=1000
+    print sumList(aList)
+    
+def main():
+    lab6()
+
+if __name__=="__main__":
+    main()
+
+wn=raw_input()
