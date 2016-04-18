23259bb20ab0daa3827341e9a74c6a0d11d5fa9b
dbwjd <dbwjd@users.noreply.github.com>
2016-04-18 23:11:59 +0900

    4-18.사각형좌표값저장

---
+++ W7main.py
@@ -0,0 +1,27 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawSquareAt(size, pos):
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    tracks=list()
+    for i in range(0,4):
+        tracks.append(t1.pos())
+        t1.forward(size)
+        t1.right(90)
+    return tracks
+
+def lap7():
+    mytrack=drawSquareAt(100,(20,30))
+    print mytrack
+    
+def main():
+    lap7()
+
+
+if __name__=="__main__":
+    main()
+
+wn=raw_input()
