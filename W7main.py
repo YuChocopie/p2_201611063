7c7f10425c4352a057434ef76a0fbb7b3e3e58de
dbwjd <dbwjd@users.noreply.github.com>
2016-04-20 00:21:19 +0900

    Update W7main.py

--- W7main.py
+++ W7main.py
@@ -13,9 +13,21 @@ def drawSquareAt(size, pos):
         t1.right(90)
     return tracks
 
+def drawSquareFrom():
+    t1.penup()
+    t1.home()
+    t1.pendown()
+    tracks=({0:(0,0),1:(50,0),2:(50,50),3:(0,50),4:(0,0)})
+    
+    for i in range(1,5):
+        t1.goto(tracks[i])
+    for i in range(1,5):
+        print tracks[i]
+
 def lap7():
     mytrack=drawSquareAt(100,(20,30))
     print mytrack
+    drawSquareFrom()
     
 def main():
     lap7()
