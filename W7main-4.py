1e56772a245e3e314a712fd80acb86ca9044b6cc
dbwjd <dbwjd@users.noreply.github.com>
2016-04-20 14:15:22 +0900

    4.20  거북이미로

---
+++ W7main-4.py
@@ -0,0 +1,84 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.pencolor("RED")
+t1.shape("turtle")
+
+
+def saveTracks():
+    t1.penup()
+    mytrack=list()
+
+    t1.goto(-400,300)
+    t1.pendown()
+
+    mytrack.append(t1.pos())
+
+    t1.left(90)
+    t1.left(90)
+    t1.left(90)
+    t1.forward(80)
+    t1.forward(20)
+    t1.forward(20)
+
+    mytrack.append(t1.pos())
+
+
+    t1.left(90)
+    t1.forward(100)
+    t1.forward(100)
+    t1.forward(100)
+
+    mytrack.append(t1.pos())
+
+    t1.left(90)
+    t1.left(90)
+    t1.left(90)
+    t1.forward(100)
+    t1.forward(100)
+    t1.forward(20)
+    t1.forward(20)
+    t1.forward(20)
+
+    mytrack.append(t1.pos())
+
+    t1.left(90)
+    t1.forward(100)
+    t1.forward(100)
+    t1.forward(20)
+
+    mytrack.append(t1.pos())
+
+    t1.left(90)
+    t1.left(180)
+
+    t1.forward(100)
+
+    mytrack.append(t1.pos())
+
+
+    t1.left(90)
+    t1.forward(100)
+    t1.forward(100)
+
+    mytrack.append(t1.pos())
+
+
+def replayTracks(mytrack):
+    for t in mytrack:
+        print t
+
+def lap7():
+    mytrack=saveTracks()
+    replayTracks(mytrack)
+
+def main():
+    lap7()
+
+if __name__=="__main__":
+    main()
+
+main()
+raw_input('ㅎㅎ')
+
+wn.exitonclick()
