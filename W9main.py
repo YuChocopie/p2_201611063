7078b07848f5c8328753fb786affe1aa122f48d2
dbwjd <dbwjd@users.noreply.github.com>
2016-05-02 15:17:00 +0900

    상명대학교 문자 수 세기

---
+++ W9main.py
@@ -0,0 +1,30 @@
+﻿def charCount():
+    sentence='sangmyung university'
+    print list(sentence)
+    allchars= list(sentence)
+    for c in allchars:
+        print c,
+    d=dict()
+    for c in allchars:
+        if c not in d:
+            d[c]=1
+        else :
+            d[c]=d[c]+1
+    print d
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)), list(d.keys()))
+    plt.show()
+
+def lap9():
+    charCount()
+
+def main():
+    lap9()
+
+if __name__=="__main__":
+    main()
+
+raw_input('ㅎㅎ')
