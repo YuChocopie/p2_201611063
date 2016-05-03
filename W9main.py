c75d5e7d0c452702e518d59a9e08af32736363d2
dbwjd <dbwjd@users.noreply.github.com>
2016-05-03 18:27:46 +0900

    W9main 5,6,7 과제 문자세기

--- W9main.py
+++ W9main.py
@@ -1,4 +1,8 @@
-﻿def charCount():
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def charCount():
     sentence='sangmyung university'
     print list(sentence)
     allchars= list(sentence)
@@ -18,13 +22,62 @@
     plt.xticks(range(len(d)), list(d.keys()))
     plt.show()
 
+def countDigitsLetter(word):
+    sentence="7 hongjidong"
+    allchars= list(sentence)
+    d=dict()
+    d={"number":0, "word":0}
+    for c in word:
+        if c.isdigit()==True:
+            d["number"]=d["number"]+1
+        elif c.isdigit()==False:
+            d["word"]=d["word"]+1
+
+    print d
+
+    import matplotlib
+    import matplotlib.pyplot as plt
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)), list(d.keys()))
+    plt.show()
+def my():
+    print myhome.difference(yourhome)
+def you():
+    print yourhome.difference(myhome)
+def together():
+    print myhome.intersection(yourhome)
+def All():
+    print myhome.union(yourhome)
+def count():
+    d=dict()
+    for c in myhome:
+        if c not in d:
+            d[c]=1
+        else :
+            d[c]=d[c]+1
+    for c in yourhome:
+        if c not in d:
+            d[c]=1
+        else :
+            d[c]=d[c]+1
+
+
+
 def lap9():
     charCount()
+    word=raw_input("input word:")
+    countDigitsLetter(word)
+    my()
+    you()
+    together()
+    All()
+    count
 
 def main():
     lap9()
 
+
 if __name__=="__main__":
     main()
 
-raw_input('ㅎㅎ')
+wn=raw_input()
